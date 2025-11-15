310490b7fcf60951fc25ee49dec40512a60eed25
SebastianCanoOlarte <scanoo@unal.edu.co>
2025-11-15 11:28:25 -0500

    intento 3

--- homework/pregunta_12.py
+++ homework/pregunta_12.py
@@ -4,7 +4,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import pandas as pd
 
 def pregunta_12():
     """
@@ -22,3 +22,7 @@ def pregunta_12():
     38   38                    eee:0,fff:9,iii:2
     39   39                    ggg:3,hhh:8,jjj:5
     """
+    tbl2 = pd.read_csv("files/input/tbl2.tsv", sep="\t")
+    tbl2['c5'] = tbl2['c5a'] + ':' + tbl2['c5b'].astype(str)
+    result = tbl2.groupby('c0')['c5'].apply(lambda x: ','.join(sorted(x))).to_frame().reset_index()
+    return result
